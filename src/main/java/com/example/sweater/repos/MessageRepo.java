61d79c84873b9a9ec9aa1a37cd115a5f18735d09
mike.goolev <mike.goolev@seasoned.co>
2019-12-05 00:35:50 -0600

    New feature - filtering by tag

--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -3,5 +3,9 @@ package com.example.sweater.repos;
 import com.example.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
 }
