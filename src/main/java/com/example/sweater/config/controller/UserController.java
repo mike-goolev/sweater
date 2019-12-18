f9eb1957d7f98e1d72866ccd779bfc656af8b380
mike.goolev <mike.goolev@seasoned.co>
2019-12-17 18:02:32 -0600

    Added feature for displaying user roles as checkboxes

--- src/main/java/com/example/sweater/config/controller/UserController.java
+++ src/main/java/com/example/sweater/config/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.sweater.config.controller;
 
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,12 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
     }
 }
---
+++ src/main/resources/templates/userEdit.ftl
@@ -0,0 +1,18 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+User Editor
+
+    <form acttion="/user" method="post">
+        <input type="text" name="username" value="${user.username}">
+        <#list roles as role>
+            <div>
+                <label><input type="checkbox" name="${role}" ${user.roles?seq_contains(role)?string("checked", "")}>${role}</label>
+            </div>
+        </#list>
+        <input type="text" value="${user.id}" name="userId">
+        <input type="text" value="${_csrf.token}" name="_csrf">
+        <button type="submit">Save</button>
+
+    </form>
+</@c.page>
