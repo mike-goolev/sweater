e244223ac8c5152de4e795af006575f4a58dbb28
mike.goolev <mike.goolev@seasoned.co>
2019-12-28 23:59:41 -0600

    Started refactoring for moving addUser method to UserService

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -16,4 +16,8 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
     }
+
+    public void addUser() {
+
+    };
 }
