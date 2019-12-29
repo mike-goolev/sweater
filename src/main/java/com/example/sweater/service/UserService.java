32fdfe1898bbaa969ff02ec739d67293ece80878
mike.goolev <mike.goolev@seasoned.co>
2019-12-29 14:34:58 -0600

    created new MailSender class

--- src/main/java/com/example/sweater/service/UserService.java
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -1,15 +1,14 @@
 package com.example.sweater.service;
 
-import antlr.StringUtils;
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
-import freemarker.template.utility.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Collections;
 import java.util.UUID;
@@ -18,6 +17,9 @@ import java.util.UUID;
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private MailSender mailSender;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -36,6 +38,16 @@ public class UserService implements UserDetailsService {
         user.setActivateCode(UUID.randomUUID().toString());
 
         userRepo.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s \n" + "Welcome to Sweater. To activate your account follow the link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivateCode()
+            );
+
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
 
         return true;
     }
