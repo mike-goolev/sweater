933b0ba3d6d7576638f12bf028f8f3ffceec5b2b
mike.goolev <mike.goolev@seasoned.co>
2019-12-30 22:22:15 -0600

    updated types, length and syntax of constraints in migration file

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,7 +8,7 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String text;
     private String tag;
@@ -33,11 +33,11 @@ public class Message {
         return author != null ? author.getUsername() : "<none>"; // boolean statement ? true result : false result;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/resources/db.migrate/V1__Init_DB.sql
+++ src/main/resources/db.migrate/V1__Init_DB.sql
@@ -1,10 +1,10 @@
 create sequence hibernate_sequence start 1 increment 1;
 
 create table message (
-    id int4 not null,
+    id int8 not null,
     filename varchar(255),
     tag varchar(255),
-    text varchar(255),
+    text varchar(2048) not null,
     user_id int8,
     primary key (id)
 );
@@ -19,15 +19,15 @@ create table usr (
     activation_code varchar(255),
     active boolean not null,
     email varchar(255),
-    password varchar(255),
-    username varchar(255),
+    password varchar(255) not null,
+    username varchar(255) not null,
     primary key (id)
 );
 
 alter table if exists message
-    add constraint FK70bv6o4exfe3fbrho7nuotopf
+    add constraint message_user_fk
     foreign key (user_id) references usr;
 
 alter table if exists user_role
-    add constraint FKfpm8swft53ulq2hl11yplpr5
+    add constraint user_role_fk
     foreign key (user_id) references usr;
