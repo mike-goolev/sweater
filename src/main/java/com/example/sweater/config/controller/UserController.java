6609b0490572f26e6bfe771ced80a25cd054c956
mike.goolev <mike.goolev@seasoned.co>
2020-01-02 19:33:30 -0600

    Set up checking of password during user login

--- src/main/java/com/example/sweater/config/controller/UserController.java
+++ src/main/java/com/example/sweater/config/controller/UserController.java
@@ -28,8 +28,8 @@ public class UserController {
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
-        model.addAttribute("roles", Role.values());
         model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
 
         return "userEdit";
     }
@@ -63,6 +63,4 @@ public class UserController {
 
         return "redirect:/user/profile";
     }
-
-
 }
