50b9772ec3baed6384411c7f9653dfb697b85d8a
mike.goolev <mike.goolev@seasoned.co>
2020-01-03 21:45:10 -0600

    Implemented password confirmation field and its getter/setter

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -19,6 +19,9 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
+    @Transient
+    @NotBlank(message = "Password confirmation cannot be empty")
+    private String password2;
     private boolean active;
 
     @NotBlank(message = "Email cannot be empty")
@@ -114,5 +117,13 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activateCode) {
         this.activationCode = activateCode;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
     }
 }
