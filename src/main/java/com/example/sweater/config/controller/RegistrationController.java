b72fc465d699ec0ac71c79dbd3ac5814d3deed8d
mike.goolev <mike.goolev@seasoned.co>
2020-01-08 15:14:56 -0600

    Fixed register confirmation 500 error

--- src/main/java/com/example/sweater/config/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/config/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -24,13 +26,23 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+                          BindingResult bindingResult,
+                          Model model) {
+        //validate password confirmation
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+
         //validate password and password2
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
             model.addAttribute("passwordError", "Confirmation password do not match");
         }
 
-        if (bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtills.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
