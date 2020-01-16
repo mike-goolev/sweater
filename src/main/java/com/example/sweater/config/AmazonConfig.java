2db68c40db21b7632682a182f8e4bdce90905b47
mike.goolev <mike.goolev@seasoned.co>
2020-01-16 01:47:50 -0600

    Implemented Amazon s3 client with access and secret key from AWS

---
+++ src/main/java/com/example/sweater/config/AmazonConfig.java
@@ -0,0 +1,25 @@
+package com.example.sweater.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIAJGOYNHMMCP4QLIZQ",
+                "QLBvxaoYuFIOvC1EsBATjjnhMK37/5ELLt4wl9PJ"
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
